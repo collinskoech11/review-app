3bcf65fcd1594a4efd6c8ed55d2103e345610ed4
collinskoech11 <collinskoechck34@gmail.com>
2021-02-21 08:01:53 +0300

    update

--- review/src/App.js
+++ review/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import NavPage from './Components/NavPage'
+import React  from 'react';
+import NavPage from './Components/NavPage';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
+      
      <NavPage/>
+     <h1>Hello World</h1>
     </div>
   );
 }
