e85ae8b562335140fc53f8448277f42b7ddf52e2
collinskoech11 <collinskoechck34@gmail.com>
2021-02-21 07:19:20 +0300

    react v 16

--- review/src/App.js
+++ review/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import NavPage from './Components/NavPage'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <NavPage/>
     </div>
   );
 }
