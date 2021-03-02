63f1e0f451a7fd0eb239bda5b68c3c356be9fc5e
Collins Kiptoo <53892938+collinskandie@users.noreply.github.com>
2021-03-02 23:14:51 +0300

    added about us page

---
+++ review/src/Pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1><strong>Welcome to Developers Student Club, CUEA</strong></h1>
+            <h2>To learn more about us, click the following link <a href="https://developers.google.com/community/dsc">Developer Student Clubs</a></h2>
+            <h3>Helping students bridge the gap between theory and practice</h3>
+            <p>Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a DSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.</p>
+
+        </div>
+    )
+}
+
+export default About
--- review/src/Pages/Home.js
+++ review/src/Pages/Home.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            <h1>Welcome to Developers Student Club, Cuea</h1>
+            <h1>Welcome to Developers Student Club, CUEA</h1>
+            <p> <a href="./Pages/About.js">Learn more</a></p>
 
         </div>
     )
