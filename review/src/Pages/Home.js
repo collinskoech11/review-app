72fdb8915bd8178027df9558ab26586ff76ac97d
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-03-06 17:39:00 +0300

    Update Home.js

--- review/src/Pages/Home.js
+++ review/src/Pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            <h1>Welcome to Developers Student Club, Cuea</h1>
+            <h1>Welcome to Developers Student Club of CUEA</h1>
 
         </div>
     )
